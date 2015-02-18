c27ab01a5f94367dda4d022d7b0366879030b75c
Giuseppe Roselli <GRoselli@MacBook-Pro-di-Giuseppe.local>
2015-02-18 15:36:00 +0100

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Giuseppe Roselli on 18/02/15.
+//  Copyright (c) 2015 Roselli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Giuseppe Roselli on 18/02/15.
+//  Copyright (c) 2015 Roselli. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
